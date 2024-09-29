683ff9870a43c8333ab326fca26d63348b501215
Rosa M Perez <rosaperezlarios@gmail.com>
2024-09-29 11:51:41 -0700

    switched screens for Login layout

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,32 +1,35 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import React from 'react';
+import { ImageBackground, StyleSheet,View, } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground
+            style={styles.background}
+            source={require("../assets/images/background.jpeg")}
+        >
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-        
-       
-    </ParallaxScrollView>
-  );
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#fc5c65',
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: 'green',
+
+    },
+})
+
+export default WelcomeScreen;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,53 +8,23 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import WelcomeScreen from "../screens/WelcomeScreen";
 
 export default function HomeScreen() {
-  const handleImagePress = () => {
-    // Action when image is clicked, e.g., navigate to another screen or show an alert
-    console.log("Image clicked!");
-  };
-  const handlePress = () => console.log("text pressed")
   return (
     <>
-    <WelcomeScreen/>
+     
       <ParallaxScrollView
         headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
         headerImage={
-          <TouchableOpacity onPress={handleImagePress}>
-            <Image source={require("@/assets/images/RP_my_logo.png")} 
-              // style={styles.logoImage} // Resizing the header image
-            />
-          </TouchableOpacity>
+          <Image source={require("../assets/images/RP_my_logo.png")} />
         }
       >
         <ThemedView style={styles.titleContainer}></ThemedView>
       </ParallaxScrollView>
       <View style={styles.container}>
-        <Text numberOfLines={1} onPress={handlePress}>hello a really really long textreally really long text really really long text really really long text
-        </Text>
-        <TouchableOpacity onPress={() => console.log("Image tapped")}>
-        <Image 
-        source={{
-          width: 200,
-          height: 300, 
-          uri: "https://picsum.photos/200/300"}}
-        // source={require('@/assets/images/icon.png')}
-        // style={styles.iconImage} // Resizing the icon image
-        /></TouchableOpacity>
-        <SafeAreaView>
-        <Button 
-        color="orange"
-        title="Click Me" 
-        onPress={() => Alert.alert("My title", "My message", [
-          {text: "Yes", onPress:( ) => console.log("Yes")},
-          {text: "No", onPress:( ) => console.log("Yes")},
-
-        ])} /></SafeAreaView>
-        {/* </Button> */}
+        <Text>hello </Text>
       </View>
     </>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -71,13 +41,6 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  // reactLogo: {
-  //   height: 178,
-  //   width: 290,
-  //   bottom: 0,
-  //   left: 0,
-  //   position: "absolute",
-  // },
   logoImage: {
     width: 150, // Set desired width for logo image
     height: 150, // Set desired height for logo image
